c0047088a5ff42504959ec497f83d4d3bd8dbcd7
fermelli <luisfernandosalgadomiguez@gmail.com>
2021-05-31 18:33:38 -0400

    Servicio Web C# (SOAP C#) con clientes en C# y Java

---
+++ ClienteServicioWebCSharp/ClienteServicioWebCSharp/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ClienteServicioWebCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.btnRestar = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(161, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "valor de A:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(161, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "valor de B:";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(164, 74);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(208, 20);
+            this.txtA.TabIndex = 2;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(164, 146);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(208, 20);
+            this.txtB.TabIndex = 3;
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.Location = new System.Drawing.Point(41, 211);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(75, 23);
+            this.btnSumar.TabIndex = 4;
+            this.btnSumar.Text = "Sumar";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            this.btnSumar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnRestar
+            // 
+            this.btnRestar.Location = new System.Drawing.Point(164, 211);
+            this.btnRestar.Name = "btnRestar";
+            this.btnRestar.Size = new System.Drawing.Size(75, 23);
+            this.btnRestar.TabIndex = 5;
+            this.btnRestar.Text = "Restar";
+            this.btnRestar.UseVisualStyleBackColor = true;
+            this.btnRestar.Click += new System.EventHandler(this.btnRestar_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Location = new System.Drawing.Point(297, 211);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiplicar.TabIndex = 6;
+            this.btnMultiplicar.Text = "Multiplicar";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Location = new System.Drawing.Point(410, 211);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(75, 23);
+            this.btnDividir.TabIndex = 7;
+            this.btnDividir.Text = "Dividir";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(161, 277);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(64, 13);
+            this.lblResultado.TabIndex = 8;
+            this.lblResultado.Text = "Resultado: -";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 332);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnRestar);
+            this.Controls.Add(this.btnSumar);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Button btnSumar;
+        private System.Windows.Forms.Button btnRestar;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ ClienteServicioWebCSharp/ClienteServicioWebCSharp/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClienteServicioWebCSharp
+{
+    public partial class Form1 : Form
+    {
+        int a;
+        int b;
+        float resultado;
+        ServicioOperaciones.OperacionesSoapClient operaciones;
+
+        public Form1()
+        {
+            InitializeComponent();
+            operaciones = new ServicioOperaciones.OperacionesSoapClient();
+        }
+
+        private void leerValores()
+        {
+            a = int.Parse(txtA.Text);
+            b = int.Parse(txtB.Text);
+        }
+
+        private void imprimirResultado(String mensaje)
+        {
+            lblResultado.Text = mensaje;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            leerValores();
+
+            resultado = operaciones.Suma(a, b);
+
+            imprimirResultado("Resultado: " + resultado.ToString());
+        }
+
+        private void btnRestar_Click(object sender, EventArgs e)
+        {
+            leerValores();
+
+            resultado = operaciones.Resta(a, b);
+
+            imprimirResultado("Resultado: " + resultado.ToString());
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            leerValores();
+
+            resultado = operaciones.Multiplicacion(a, b);
+
+            imprimirResultado("Resultado: " + resultado.ToString());
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            leerValores();
+
+            if (b != 0)
+            {
+                resultado = operaciones.Division(a, b);
+
+                imprimirResultado("Resultado: " + resultado.ToString());
+            }
+            else
+            {
+                imprimirResultado("No se puede dividir entre 0");
+            }
+
+
+        }
+    }
+}
